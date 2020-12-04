400284de8b7d2ec57cbbc6fc4279adf217a2e770
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-03 21:35:19 -0700

    Three test cases for valid password check

---
+++ src/main/java/day_02/Day02.java
@@ -0,0 +1,6 @@
+
+public class Day02 {
+    public static boolean isPasswordValid(int min, int max, char character, String password){
+        return false;
+    }
+}
---
+++ src/test/java/day_02/Day02Tests.java
@@ -0,0 +1,29 @@
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class Day02Tests {
+
+    @Test
+    @DisplayName("Password \"abcde\" should be valid for 1-3 'a' characters")
+    void Abcde_Test(){
+        var isValid = Day02.isPasswordValid(1,3,'a',"abcde");
+        assertTrue(isValid);
+    }
+
+    @Test
+    @DisplayName("Password \"cdefg\" should not be valid for 1-3 'b' characters")
+    void Cdefg_Test(){
+        var isValid = Day02.isPasswordValid(1,3,'b',"cdefg");
+        assertFalse(isValid);
+    }
+
+    @Test
+    @DisplayName("Password \"ccccccccc\" should be valid for 2-9 'c' characters")
+    void Multiple_C_Test(){
+        var isValid = Day02.isPasswordValid(1,3,'c',"ccccccccc");
+        assertTrue(isValid);
+    }
+}
