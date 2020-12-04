d3c631dbf558047bb5a88a200c3b76efe02625a0
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-03 19:28:45 -0700

    Pass first test

--- src/main/java/day_01/Day01.java
+++ src/main/java/day_01/Day01.java
@@ -2,8 +2,17 @@ import java.util.List;
 
 class Day01 {
     public static int productOfDaysAddingTo2020(List<Integer> list){
-        int hey = list.stream().filter(x -> x == 299).
-            findFirst().orElse(0);
-        return hey;
+        var listIterator = list.listIterator();
+        while(listIterator.hasNext()){
+            var firstNum = listIterator.next();
+            int secondNum = list.stream().
+                filter(x -> x + firstNum == 2020).
+                findFirst().
+                orElse(0);
+            if(secondNum != 0){
+                return firstNum * secondNum;
+            }
+        }
+        return 0;
     }
 }
