39707fe5711f15e23b824b453d55e47f78380ff7
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-03 20:01:37 -0700

    Remove print to console tests

--- src/test/java/day_01/Day01Tests.java
+++ src/test/java/day_01/Day01Tests.java
@@ -24,26 +24,10 @@ public class Day01Tests {
     }
 
     @Test
-    void Print_Part_1_Answer_To_Console() throws Exception{
-        var path = Paths.get("src/test/java/day_01/day_01_input.txt");
-        List<Integer> intList = PuzzleInputReader.readIntListFromFile(path);
-        var result = Day01.productOfDaysAddingTo2020(intList);
-        System.out.println(result);
-    }
-
-    @Test
     @DisplayName("List with 979, 366, 675 should return 241861950")
     void Three_Expense_Product_Test(){
         var expenseList = List.of(1721,979,366,299,675,1456);
         var result = Day01.productOf3DaysAddingTo2020(expenseList);
         assertEquals(241861950,result);
-    }
-
-    @Test
-    void Print_Part_2_Answer_To_Console() throws Exception{
-        var path = Paths.get("src/test/java/day_01/day_01_input.txt");
-        List<Integer> intList = PuzzleInputReader.readIntListFromFile(path);
-        var result = Day01.productOf3DaysAddingTo2020(intList);
-        System.out.println(result);
     }
 }
