ffad6df4e9a741c8a9c3b10a16ff72508bd915ee
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-03 21:49:46 -0700

    Pass two of the tests

--- src/main/java/day_02/Day02.java
+++ src/main/java/day_02/Day02.java
@@ -1,6 +1,9 @@
 
 public class Day02 {
     public static boolean isPasswordValid(int min, int max, char character, String password){
-        return false;
+        int characterCount = password.chars()
+            .reduce(0,
+                (a, b) -> a + ((char) b == character ? 1 : 0));
+        return min <= characterCount && characterCount <= max;
     }
 }
