cbb2836cad54c30ad59f3ce003172217f6e02885
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-03 19:19:07 -0700

    Create better test

---
+++ src/main/java/day_01/Day01.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+class Day01 {
+    public static int productOfDaysAddingTo2020(List<Integer> list){
+        return 0;
+    }
+}
---
+++ src/test/java/day_01/Day01Tests.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class Day01Tests {
+    @Test
+    @DisplayName("List with 1721 and 299 should return 514579")
+    void First_Product_Test(){
+        var expenseList = List.of(1721,979,366,299,675,1456);
+        var result = Day01.productOfDaysAddingTo2020(expenseList);
+        assertEquals(514579,result);
+    }
+}
