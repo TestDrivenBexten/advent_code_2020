7610b70090237e41f741f92d1daf1b605117ba3c
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-04 20:52:49 -0700

    Write failing test for double collision

--- src/test/java/day_03/Day03Tests.java
+++ src/test/java/day_03/Day03Tests.java
@@ -12,4 +12,13 @@ public class Day03Tests {
         var collisionCount = terrain.calculateCollisionsPerSlope(1,1);
         assertEquals(1,collisionCount);
     }
+
+    @Test
+    @DisplayName("Terrain with trees in 1,1 and 2,2 should have two collisions for slope 1-1")
+    void Double_Tree_Test(){
+        boolean treeArray[][] = {{false,false,false},{false,true,false},{false,false,true}};
+        var terrain = new TreeTerrain(treeArray);
+        var collisionCount = terrain.calculateCollisionsPerSlope(1,1);
+        assertEquals(2,collisionCount);
+    }
 }
