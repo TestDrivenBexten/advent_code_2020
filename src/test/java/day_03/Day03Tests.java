07952386f6c363973534c66f413ff7c0d7481c9e
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-04 21:25:52 -0700

    Failing overflow patterns

--- src/test/java/day_03/Day03Tests.java
+++ src/test/java/day_03/Day03Tests.java
@@ -17,12 +17,27 @@ public class Day03Tests {
     }
 
     @Test
-    @DisplayName("Terrain with trees in 1,1 and 2,2 should have two collisions for slope 1-1")
+    @DisplayName("Terrain with trees in 1,1 and 2,2 should repeat pattern for slope 1-1")
     void Double_Tree_Test(){
         boolean treeArray[][] = {{false,false,false},{false,true,false},{false,false,true}};
         var terrain = new TreeTerrain(treeArray);
         var collisionCount = terrain.calculateCollisionsPerSlope(1,1);
         assertEquals(2,collisionCount);
+    }
+
+    @Test
+    @DisplayName("Terrain with of height 5 and width 2 should have two collisions for slope 1-1")
+    void Overflow_Terrain_Test(){
+        boolean treeArray[][] = {
+            {false,false},
+            {false,true},
+            {true,false},
+            {false,true},
+            {false,false}
+        };
+        var terrain = new TreeTerrain(treeArray);
+        var collisionCount = terrain.calculateCollisionsPerSlope(1,1);
+        assertEquals(4,collisionCount);
     }
 
     @Test
