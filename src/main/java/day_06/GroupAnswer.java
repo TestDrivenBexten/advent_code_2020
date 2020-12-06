a4d59260f789d113c14ee6ae3ccc8b03a451c81f
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-06 12:08:04 -0700

    Count unanimous answers for groups

--- src/main/java/day_06/GroupAnswer.java
+++ src/main/java/day_06/GroupAnswer.java
@@ -6,14 +6,37 @@ import java.util.Set;
 public class GroupAnswer {
     public static int calculateGroupAnswerCount(String groupAnswer){
         char[] answerArray = groupAnswer.replaceAll("\n", "").toCharArray();
-        Set<Character> uniqueAnswerSet = new HashSet<Character>();
-        for(char answer: answerArray){
-            uniqueAnswerSet.add(answer);
-        }
+        var uniqueAnswerSet = uniqueCharacterSet(answerArray);
+
         return uniqueAnswerSet.size();
     }
 
     public static int calculateUnanimousGroupAnswerCount(String groupAnswer){
-        return -1;
+        String[] personArray = groupAnswer.split("\n");
+
+        char[] answerArray = groupAnswer.replaceAll("\n", "").toCharArray();
+        var uniqueAnswerSet = uniqueCharacterSet(answerArray);
+
+        int unanimousCount = 0;
+        boolean isUnanimous;
+        for(char answer: uniqueAnswerSet){
+            isUnanimous = true;
+            for(String person: personArray){
+                isUnanimous &= person.indexOf(answer) != -1;
+            }
+            if(isUnanimous){
+                unanimousCount++;
+            }
+        }
+
+        return unanimousCount;
+    }
+
+    private static Set<Character> uniqueCharacterSet(char[] charArray){
+        Set<Character> uniqueCharSet = new HashSet<Character>();
+        for(char character: charArray){
+            uniqueCharSet.add(character);
+        }
+        return uniqueCharSet;
     }
 }
