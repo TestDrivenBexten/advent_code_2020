097998489c5dd19a2f9ed71415ac1e6fcde93e78
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-06 21:37:06 -0700

    Convert Java parameterized test to Kotlin test

--- src/test/java/day_06/GroupAnswerTests.java
+++ src/test/java/day_06/GroupAnswerTests.java
@@ -1,37 +1,14 @@
 package day_06;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import util.PuzzleInputReader;
 
 class GroupAnswerTests {
-
-  @ParameterizedTest
-  @MethodSource("groupAnswerCountProvider")
-  @DisplayName("Check group answer counts")
-  void Group_Answer_Count_Tests(String groupAnswer, int expectedCount) {
-    int actualCount = GroupAnswer.calculateGroupAnswerCount(groupAnswer);
-    assertEquals(expectedCount, actualCount);
-  }
-
-  static Stream<Arguments> groupAnswerCountProvider() {
-    return Stream.of(
-        arguments("abc", 3),
-        arguments("a\nb\nc", 3),
-        arguments("ab\nac", 3),
-        arguments("a\na\na\na", 1),
-        arguments("b", 1));
-  }
-
   @Test
   @DisplayName("Should count x answers from groups")
   void Sum_Group_Answer_Test() throws Exception {
--- src/test/kotlin/day_06/GroupAnswerKotlinTests.kt
+++ src/test/kotlin/day_06/GroupAnswerKotlinTests.kt
@@ -5,6 +5,19 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class GroupAnswerKotlinTests {
+    @ParameterizedTest(name = "Group spec {0} should have {1} unique answers")
+    @CsvSource(
+            "'abc', 3",
+            "'a\nb\nc', 3",
+            "'ab\nac', 3",
+            "'a\na\na\na', 1",
+            "'b', 1"
+    )
+    fun `Group Unique Answer Count Tests`(groupAnswer: String, expectedCount: Int) {
+        val actualCount = GroupAnswer.calculateGroupAnswerCount(groupAnswer)
+        assertEquals(expectedCount, actualCount)
+    }
+
     @ParameterizedTest(name = "Group spec {0} should have {1} unanimous answers")
     @CsvSource(
             "'abc', 3",
