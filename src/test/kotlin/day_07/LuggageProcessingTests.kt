7285edfc24df622bacde60712fd308ca4761e551
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-07 21:31:22 -0700

    Pass three level test

--- src/test/kotlin/day_07/LuggageProcessingTests.kt
+++ src/test/kotlin/day_07/LuggageProcessingTests.kt
@@ -28,4 +28,19 @@ class LuggageProcessingTests {
         luggageProcessor.addBagRule(secondRule);
         assertTrue(luggageProcessor.canBagFitInBag("shiny purple","drab tan"))
     }
+
+    @Test
+    fun `Shiny Blue Bag Should Hold Mirrored Black Bag Through Pale Blue Bag and Drab Tan Bag`(){
+        val firstRule = "shiny purple bags contain 2 pale blue bags, 1 wavy fuchsia bag, 5 pale salmon bags."
+        val secondRule = "pale blue bags contain 1 drab tan bag, 3 dark chartreuse bags," +
+                " 2 mirrored gold bags, 3 muted turquoise bags."
+        val thirdRule = "drab tan bags contain 4 clear gold bags, 3 mirrored black bags."
+
+        val luggageProcessor = LuggageProcessor();
+        luggageProcessor.addBagRule(firstRule);
+        luggageProcessor.addBagRule(secondRule);
+        luggageProcessor.addBagRule(thirdRule);
+
+        assertTrue(luggageProcessor.canBagFitInBag("shiny purple","mirrored black"))
+    }
 }
