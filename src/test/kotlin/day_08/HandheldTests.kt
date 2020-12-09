b9bff9c1c9e0ac906fe1360347d4ba63c91fc8a5
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-08 19:55:34 -0700

    Solve part 1

--- src/test/kotlin/day_08/HandheldTests.kt
+++ src/test/kotlin/day_08/HandheldTests.kt
@@ -20,4 +20,18 @@ class HandheldTests {
 
         assertEquals(5,accumulatedValue)
     }
+
+    @Test
+    @Throws(Exception::class)
+    fun `Puzzle Program Should End With 1217`(){
+        val path = Paths.get("src/test/kotlin/day_08/puzzle_instruction_set.txt")
+        val instructionList = PuzzleInputReader.readStringListFromFile(path)
+
+        val handheld = Handheld()
+        handheld.loadProgram(instructionList)
+        handheld.runProgram()
+        val accumulatedValue = handheld.getAccumulatedValue()
+
+        assertEquals(1217,accumulatedValue)
+    }
 }
