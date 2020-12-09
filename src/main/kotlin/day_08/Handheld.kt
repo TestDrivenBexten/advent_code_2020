725dc460846e23d9550b2bd26334569f316ee0ef
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-08 19:05:45 -0700

    Failing test for small program

---
+++ src/main/kotlin/day_08/Handheld.kt
@@ -0,0 +1,17 @@
+package day_08
+
+class Handheld {
+    private var accumulatorValue = 0
+
+    fun loadProgram(instructionList: List<String>){
+
+    }
+
+    fun runProgram(){
+
+    }
+
+    fun getAccumulatedValue(): Int{
+        return accumulatorValue
+    }
+}
---
+++ src/test/kotlin/day_08/HandheldTests.kt
@@ -0,0 +1,23 @@
+package day_08
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import util.PuzzleInputReader
+import java.nio.file.Paths
+
+class HandheldTests {
+
+    @Test
+    @Throws(Exception::class)
+    fun `Small Program Should End With 5`(){
+        val path = Paths.get("src/test/kotlin/day_08/small_instruction_set.txt")
+        val instructionList = PuzzleInputReader.readStringListFromFile(path)
+
+        val handheld = Handheld()
+        handheld.loadProgram(instructionList)
+        handheld.runProgram()
+        val accumulatedValue = handheld.getAccumulatedValue()
+
+        assertEquals(5,accumulatedValue)
+    }
+}
