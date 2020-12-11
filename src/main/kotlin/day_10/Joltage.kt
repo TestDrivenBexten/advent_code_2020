0efacccaf210a8a246b0013bb3dfc749b155a4b9
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-10 17:49:44 -0700

    Solve joltage for small input

--- src/main/kotlin/day_10/Joltage.kt
+++ src/main/kotlin/day_10/Joltage.kt
@@ -3,5 +3,20 @@ package day_10
 data class JoltageDifference(val oneJoltCount: Int, val threeJoltCount: Int)
 
 fun getJoltageDifference(joltageList: List<Int>): JoltageDifference{
-    return JoltageDifference(0,0)
+    val sortedList = joltageList.sorted().toMutableList()
+    sortedList.add(0,0)
+    sortedList.add(joltageList.last() + 3)
+
+    var oneJoltCount = 0
+    var threeJoltCount = 0
+    for (j in 0 until sortedList.size - 1){
+        val currentJoltage = sortedList[j]
+        val nextJoltage = sortedList[j + 1]
+        if(nextJoltage - currentJoltage == 1) {
+            oneJoltCount++
+        }else{
+            threeJoltCount++
+        }
+    }
+    return JoltageDifference(oneJoltCount,threeJoltCount)
 }
