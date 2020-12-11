4f86d9ba2a1617cd3352dbe70df1de6ec39e7125
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-10 20:32:32 -0700

    Remove unused code

--- src/main/kotlin/day_10/Joltage.kt
+++ src/main/kotlin/day_10/Joltage.kt
@@ -39,74 +39,11 @@ private fun getJoltageArrangementCount(joltageList: List<Int>): Long {
                 else -> 1
             }
             val sublist = joltageList.subList(j+1,joltageList.size)
-            println(sublist)
             arrangementCount *= getJoltageArrangementCount(sublist)
             break
         }
     }
     return arrangementCount
-}
-
-private fun buildArrangementList(joltageList: List<Int>): List<List<Int>>{
-    val currentJoltage = joltageList[0]
-
-    val indexOneGreater = joltageList.indexOf(currentJoltage + 1)
-    val indexTwoGreater = joltageList.indexOf(currentJoltage + 2)
-    val indexThreeGreater = joltageList.indexOf(currentJoltage + 3)
-
-    var oneArrangementList: List<List<Int>> = listOf()
-    if (indexOneGreater != -1){
-        val oneSublist = joltageList.subList(indexOneGreater,joltageList.size)
-        oneArrangementList = buildArrangementList(oneSublist)
-    }
-
-    var twoArrangementList: List<List<Int>> = listOf()
-    if (indexTwoGreater != -1){
-        val twoSublist = joltageList.subList(indexTwoGreater,joltageList.size)
-        twoArrangementList = buildArrangementList(twoSublist)
-    }
-
-    var threeArrangementList: List<List<Int>> = listOf()
-    if(indexThreeGreater != -1){
-        val threeSublist = joltageList.subList(indexThreeGreater,joltageList.size)
-        threeArrangementList = buildArrangementList(threeSublist)
-    }
-
-    oneArrangementList = oneArrangementList.map {
-        arrangementList ->
-            val mutArrangementList = arrangementList.toMutableList()
-            mutArrangementList.add(0,currentJoltage)
-            mutArrangementList
-    }
-    if (oneArrangementList.isEmpty()){
-        oneArrangementList = listOf(listOf(currentJoltage))
-    }
-    oneArrangementList = oneArrangementList.filter { it.contains(joltageList.last()) }
-
-    twoArrangementList = twoArrangementList.map {
-        arrangementList ->
-        val mutArrangementList = arrangementList.toMutableList()
-        mutArrangementList.add(0,currentJoltage)
-        mutArrangementList
-    }
-    if (twoArrangementList.isEmpty()){
-        twoArrangementList = listOf(listOf(currentJoltage))
-    }
-    twoArrangementList = twoArrangementList.filter { it.contains(joltageList.last()) }
-
-    threeArrangementList = threeArrangementList.map {
-        arrangementList ->
-        val mutArrangementList = arrangementList.toMutableList()
-        mutArrangementList.add(0,currentJoltage)
-        mutArrangementList
-    }
-    if (threeArrangementList.isEmpty()){
-        threeArrangementList = listOf(listOf(currentJoltage))
-    }
-    threeArrangementList = threeArrangementList.filter { it.contains(joltageList.last()) }
-
-    val combinedList = oneArrangementList + twoArrangementList + threeArrangementList
-    return combinedList.distinct()
 }
 
 private fun buildJoltageList(joltageList: List<Int>): List<Int>{
