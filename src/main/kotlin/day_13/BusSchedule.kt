216c6d8fab0a41c381b76e6f881b2f8afe137a61
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-13 16:57:04 -0700

    Failing test cases for small bus schedule

---
+++ src/main/kotlin/day_13/BusSchedule.kt
@@ -0,0 +1,10 @@
+package day_13
+data class Bus(val busId: Int, val timestamp: Int)
+
+fun findNextBus(currentTimestamp: Int, busIdList: List<Int>): Bus {
+    return Bus(0,0)
+}
+
+fun parseBusList(busString: String): List<Int> {
+    return listOf()
+}
---
+++ src/test/kotlin/day_13/BusScheduleTests.kt
@@ -0,0 +1,31 @@
+package day_13
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import util.PuzzleInputReader
+import java.nio.file.Paths
+
+class BusScheduleTests {
+    @Test
+    @Throws(Exception::class)
+    fun `Small Ferry Command List Should Have Manhattan Distance 25`(){
+        val path = Paths.get("src/test/kotlin/day_13/small_schedule.txt")
+        val busInput = PuzzleInputReader.readStringListFromFile(path)
+
+        val currentTimestamp = busInput[0].toInt()
+        val busIdInput = busInput[1]
+
+        val busIdList = parseBusList(busIdInput)
+
+        val (busId, departureTimestamp) = findNextBus(currentTimestamp,busIdList)
+
+        val productTimeDiffId = busId * (departureTimestamp - currentTimestamp)
+        assertAll("Check small schedule values",
+            { assertEquals(listOf(7,13,19,31,59), busIdList) },
+            { assertEquals(59, busId) },
+            { assertEquals(944, departureTimestamp)},
+            { assertEquals(295, productTimeDiffId)}
+        )
+    }
+}
