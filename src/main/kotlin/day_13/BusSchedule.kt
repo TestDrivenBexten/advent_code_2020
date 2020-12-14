8f5f30923e5da51fbfcdcc15afa1309aa81cf897
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-13 21:01:31 -0700

    Pass small bus schedule test

--- src/main/kotlin/day_13/BusSchedule.kt
+++ src/main/kotlin/day_13/BusSchedule.kt
@@ -1,4 +1,7 @@
 package day_13
+
+import java.sql.Timestamp
+
 data class Bus(val busId: Int, val timestamp: Int)
 
 fun findNextBus(currentTimestamp: Int, busIdList: List<Int>): Bus {
@@ -22,6 +25,38 @@ fun parseBusList(busString: String): List<Int> {
     return filteredIdList.map { it.toInt() }.sorted()
 }
 
+data class BusOffset(val busId: Int, val timeOffset: Int)
+
 fun findSynchronizedTimestamp(busString: String): Long {
+    val busIdStringList = busString.split(",")
+    val busOffsetList = mutableListOf<BusOffset>()
+    for(j in busIdStringList.indices){
+        val busIdString = busIdStringList[j]
+        if(busIdString != "x"){
+            val busId = busIdString.toInt()
+            busOffsetList.add(BusOffset(busId, j))
+        }
+    }
+
+    busOffsetList.sortByDescending { it.busId }
+    val (greatestId, timeOffset) = busOffsetList[0]
+
+    println(busOffsetList)
+    var targetTimestamp = 0L
+    while(targetTimestamp < 1076000L){
+        val isValidTimestamp = checkOffsetList(targetTimestamp, busOffsetList)
+        if(isValidTimestamp){
+            return targetTimestamp
+        }
+        targetTimestamp += 1
+    }
     return 0
+}
+
+private fun checkOffsetList(targetTimestamp: Long,
+                            busOffsetList: List<BusOffset>): Boolean{
+    return busOffsetList.all { busOffset ->
+        val (busId, timeOffset) = busOffset
+        (targetTimestamp + timeOffset.toLong() ) % busId == 0L
+    }
 }
