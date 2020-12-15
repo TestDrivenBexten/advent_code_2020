993b4fa318cc0ddca52159e303609b65bbec22ee
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-14 19:32:40 -0700

    Solve small bitwise program

--- src/main/kotlin/day_14/Bitwise.kt
+++ src/main/kotlin/day_14/Bitwise.kt
@@ -1,9 +1,37 @@
 package day_14
 
+import java.util.regex.Pattern
 import kotlin.math.pow
 
+data class MemoryAddress(val address: Int, var storedValue: Int)
+
 fun findMemorySumOfBitProgram(instructionList: List<String>): Int {
-    return 0
+    val memoryList = mutableListOf<MemoryAddress>()
+    val bitmask = instructionList.first().substring(7)
+
+    val memRegex = "mem\\[(\\d+)\\]\\s=\\s(\\d+)"
+    val r = Pattern.compile(memRegex)
+
+    instructionList.drop(1).map {
+        val m = r.matcher(it)
+        if(m.matches()){
+            val memoryAddress = Integer.parseInt(m.group(1))
+            val rawValue = Integer.parseInt(m.group(2))
+            val maskedValue = applyBitmask(rawValue,bitmask)
+
+            val storedMemory = memoryList.find { x ->
+                x.address == memoryAddress
+            }
+            if(storedMemory == null){
+                val memory = MemoryAddress(memoryAddress, maskedValue)
+                memoryList.add(memory)
+            }else{
+                storedMemory.storedValue = maskedValue
+            }
+        }
+    }
+
+    return memoryList.fold(0) { sum, memory -> sum + memory.storedValue }
 }
 
 fun applyBitmask(originalValue: Int, bitmask: String): Int {
