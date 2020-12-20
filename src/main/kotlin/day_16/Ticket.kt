e1ab39ca3b46299ec4825fb9fc14d8e382044d51
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-20 14:24:29 -0700

    Load ticket rule list

--- src/main/kotlin/day_16/Ticket.kt
+++ src/main/kotlin/day_16/Ticket.kt
@@ -1,20 +1,27 @@
 package day_16
 
-data class Ticket(val fieldValueList: List<Int>)
-data class TicketConfig(val yourTicket: Ticket, val nearbyTicketList: List<Ticket>)
+import java.util.regex.Pattern
 
-private val fieldRuleRegex = "(.+):\\s(\\d+)-(\\d+)\\sor\\s(\\d+)-(\\d+)"
+data class Ticket(val fieldValueList: List<Int>)
+data class FieldRange (val rangeMin: Int, val rangeMax: Int)
+data class TicketRule(val fieldName: String,
+                      val lowerRange: FieldRange,
+                      val upperRange: FieldRange)
+data class TicketConfig(val ticketRuleList: List<TicketRule>,
+                        val yourTicket: Ticket,
+                        val nearbyTicketList: List<Ticket>)
+
+private const val fieldRuleRegex = "(.+):\\s(\\d+)-(\\d+)\\sor\\s(\\d+)-(\\d+)"
 
 fun getTicketErrorRate(ticketConfig: TicketConfig): Int {
     return 0
 }
 
 fun loadTicketConfig(rawInput: List<String>): TicketConfig{
-
-
+    val ticketRuleList = readTicketRuleList(rawInput)
     val yourTicket = readYourTicket(rawInput)
     val nearbyTicketList = readNearbyTicketList(rawInput)
-    return TicketConfig(yourTicket, nearbyTicketList)
+    return TicketConfig(ticketRuleList, yourTicket, nearbyTicketList)
 }
 
 private fun readYourTicket(rawInput: List<String>): Ticket {
@@ -29,8 +36,28 @@ private fun readNearbyTicketList(rawInput: List<String>): List<Ticket> {
     return ticketInputList.map { readTicket(it) }
 }
 
-private fun readTicketRules(){
+private fun readTicketRuleList(rawInput: List<String>): List<TicketRule>{
+    val ticketRuleList = mutableListOf<TicketRule>()
 
+    val fieldRulePattern = Pattern.compile(fieldRuleRegex)
+    for(inputRow in rawInput){
+        val fieldRuleMatcher = fieldRulePattern.matcher(inputRow)
+        if(fieldRuleMatcher.matches()){
+            val fieldName = fieldRuleMatcher.group(1)
+
+            val minRangeStart = Integer.parseInt(fieldRuleMatcher.group(2))
+            val minRangeEnd = Integer.parseInt(fieldRuleMatcher.group(3))
+            val minRange = FieldRange(minRangeStart, minRangeEnd)
+
+            val maxRangeStart = Integer.parseInt(fieldRuleMatcher.group(4))
+            val maxRangeEnd = Integer.parseInt(fieldRuleMatcher.group(5))
+            val maxRange = FieldRange(maxRangeStart, maxRangeEnd)
+
+            val ticketRule = TicketRule(fieldName, minRange, maxRange)
+            ticketRuleList.add(ticketRule)
+        }
+    }
+    return ticketRuleList
 }
 
 private fun readTicket(ticketInput: String): Ticket{
