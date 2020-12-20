d2d1b4f5d9d3f4e4d72b82d37dc9e7ce128f1138
Skyler Bexten <skyler.bexten@gmail.com>
2020-12-19 20:19:09 -0700

    Getting closer

--- src/main/kotlin/day_15/Recitation.kt
+++ src/main/kotlin/day_15/Recitation.kt
@@ -1,5 +1,31 @@
 package day_15
 
 fun reciteMemory(stringInput: String, iterationCount: Int): Int{
-    return 0
+    val startingNumberList = stringInput.split(",").map { Integer.parseInt(it) }
+    var currentTurn = 1
+
+    val numberRecordMap = HashMap<Int,Int>()
+    for(index in startingNumberList.indices){
+        val startingNumber = startingNumberList[index]
+        numberRecordMap[startingNumber] = index + 1
+        currentTurn++
+    }
+
+    var numberSpoken = 0
+    currentTurn++
+    while(currentTurn <= iterationCount){
+        if(numberRecordMap.containsKey(numberSpoken)){
+            val lastTurn = numberRecordMap[numberSpoken]
+            val previousTurn = currentTurn - 1
+            val turnDiff = previousTurn - lastTurn!!
+            numberRecordMap[numberSpoken] = previousTurn
+            numberSpoken = turnDiff
+        } else {
+            numberRecordMap[numberSpoken] = currentTurn
+            numberSpoken = 0
+        }
+        currentTurn++
+    }
+
+    return numberSpoken
 }
